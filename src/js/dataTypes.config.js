e408e22f0eba85ec5c10ae140e1c03fc6c8cff27
Stefan Rink <stefan-rink@users.noreply.github.com>
2019-07-21 20:29:13 +0200

    Adjusted mx6 configuration

--- src/js/dataTypes.config.js
+++ src/js/dataTypes.config.js
@@ -1,22 +1,62 @@
 const dataTypes = {
     "tmot": {
         "translation": "tmot",
-        "id": "0",
+        "id": "2",
         "color": "#D81B60"
     },
     "tmot2": {
         "translation": "tmot2",
-        "id": "1",
+        "id": "3",
         "color": "#43A047"
+    },
+    "tfuel": {
+        "translation": "tfuel",
+        "id": "4",
+        "color": "#BA68C8"
+    },
+    "toil": {
+        "translation": "toil",
+        "id": "5",
+        "color": "#1A237E"
+    },
+    "toil2": {
+        "translation": "toil2",
+        "id": "6",
+        "color": "#F44336"
+    },
+    "ub": {
+        "translation": "ub",
+        "id": "7",
+        "color": "#4A148C"
+    },
+    "fuelcons": {
+        "translation": "fuelcons",
+        "id": "8",
+        "color": "#5C6BC0"
+    },
+    "pbrake_f": {
+        "translation": "p_brake_f",
+        "id": "9",
+        "color": "#26C6DA"
     },
     "p_brake_r": {
         "translation": "p_brake_r",
-        "id": "2",
-        "color": "#1A237E"
+        "id": "10",
+        "color": "#26A69A"
     },
-    "shift": {
-        "translation": "Gang",
-        "id": "3",
-        "color": "#FF6F00"
+    "pclutch": {
+        "translation": "pclutch",
+        "id": "11",
+        "color": "#F4511E"
+    },
+    "pfuel": {
+        "translation": "pfuel",
+        "id": "12",
+        "color": "#F9A825"
+    },
+    "poil": {
+        "translation": "poil",
+        "id": "13",
+        "color": "#6D4C41"
     },
 };
