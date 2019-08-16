65f3f9e086d84f1391acc41637619945b98c1e86
Stefan Rink <stefan-rink@users.noreply.github.com>
2019-08-16 16:41:05 +0200

    Updated data-types config for al19 at Barcelona

--- src/js/dataTypes.config.js
+++ src/js/dataTypes.config.js
@@ -20,7 +20,7 @@ const dataTypes = {
         "color": "#1A237E"
     },
     "toil2": {
-        "translation": "toil2",
+        "translation": "tecu_sys",
         "id": "6",
         "color": "#F44336"
     },
@@ -29,8 +29,8 @@ const dataTypes = {
         "id": "7",
         "color": "#4A148C"
     },
-    "fuelcons": {
-        "translation": "fuelcons",
+    "B_engon": {
+        "translation": "B_engon",
         "id": "8",
         "color": "#5C6BC0"
     },
@@ -57,6 +57,186 @@ const dataTypes = {
     "poil": {
         "translation": "poil",
         "id": "13",
+        "color": "#6D4C41"
+    },
+    "ath": {
+        "translation": "ath",
+        "id": "14",
+        "color": "#D81B60"
+    },
+    "aps": {
+        "translation": "aps",
+        "id": "15",
+        "color": "#43A047"
+    },
+    "cutoff_gc_ems": {
+        "translation": "cutoff_gc_ems",
+        "id": "16",
+        "color": "#BA68C8"
+    },
+    "gear": {
+        "translation": "gear",
+        "id": "17",
+        "color": "#1A237E"
+    },
+    "fan_pwn_ems": {
+        "translation": "fan_pwm_ems",
+        "id": "18",
+        "color": "#F44336"
+    },
+    "fuelcons": {
+        "translation": "fuelcons",
+        "id": "19",
+        "color": "#4A148C"
+    },
+    "disttrip": {
+        "translation": "disttrip",
+        "id": "20",
+        "color": "#5C6BC0"
+    },
+    "dam_fl": {
+        "translation": "dam_fl",
+        "id": "21",
+        "color": "#26C6DA"
+    },
+    "dam_fr": {
+        "translation": "dam_fr",
+        "id": "22",
+        "color": "#26A69A"
+    },
+    "dam_rr": {
+        "translation": "dam_rr",
+        "id": "23",
+        "color": "#F4511E"
+    },
+    "dam_rl": {
+        "translation": "dam_rl",
+        "id": "24",
+        "color": "#F9A825"
+    },
+    "accy": {
+        "translation": "accy",
+        "id": "25",
+        "color": "#6D4C41"
+    },
+    "steer": {
+        "translation": "steer",
+        "id": "26",
+        "color": "#D81B60"
+    },
+    "speed": {
+        "translation": "speed",
+        "id": "27",
+        "color": "#43A047"
+    },
+    "accx": {
+        "translation": "accx",
+        "id": "28",
+        "color": "#BA68C8"
+    },
+    "nmot": {
+        "translation": "nmot",
+        "id": "29",
+        "color": "#1A237E"
+    },
+    "lambda": {
+        "translation": "lambda",
+        "id": "30",
+        "color": "#F44336"
+    },
+    "flc": {
+        "translation": "flc",
+        "id": "31",
+        "color": "#4A148C"
+    },
+    "B_lsuop": {
+        "translation": "B_lsuop",
+        "id": "32",
+        "color": "#5C6BC0"
+    },
+    "B_launchsw": {
+        "translation": "B_launchsw",
+        "id": "33",
+        "color": "#26C6DA"
+    },
+    "B_autoshiftsw": {
+        "translation": "B_autoshiftsw",
+        "id": "34",
+        "color": "#26A69A"
+    },
+    "B_clutch_open": {
+        "translation": "B_clutch_open",
+        "id": "35",
+        "color": "#F4511E"
+    },
+    "B_upshevent_ems": {
+        "translation": "B_upshevent_ems",
+        "id": "36",
+        "color": "#F9A825"
+    },
+    "mi_launch": {
+        "translation": "mi_launch",
+        "id": "37",
+        "color": "#6D4C41"
+    },
+    "vwheel_fl": {
+        "translation": "vwheel_fl",
+        "id": "38",
+        "color": "#D81B60"
+    },
+    "vwheel_fr": {
+        "translation": "vwheel_fr",
+        "id": "39",
+        "color": "#43A047"
+    },
+    "vwheel_rl": {
+        "translation": "vwheel_rl",
+        "id": "40",
+        "color": "#BA68C8"
+    },
+    "vwheel_rr": {
+        "translation": "vwheel_rr",
+        "id": "41",
+        "color": "#1A237E"
+    },
+    "slip_act": {
+        "translation": "slip_act",
+        "id": "42",
+        "color": "#F44336"
+    },
+    "gc_state_ems": {
+        "translation": "gc_state_ems",
+        "id": "43",
+        "color": "#4A148C"
+    },
+    "??????": {
+        "translation": "??????",
+        "id": "44",
+        "color": "#5C6BC0"
+    },
+    "upshifterrcnt": {
+        "translation": "upshifterrcnt",
+        "id": "45",
+        "color": "#26C6DA"
+    },
+    "dnshifterrcnt": {
+        "translation": "dnshifterrcnt",
+        "id": "46",
+        "color": "#26A69A"
+    },
+    "shiftup_pwm_ems": {
+        "translation": "shiftup_pwm_ems",
+        "id": "47",
+        "color": "#F4511E"
+    },
+    "shiftdn_pwm_ems": {
+        "translation": "shiftdn_pwm_ems",
+        "id": "48",
+        "color": "#F9A825"
+    },
+    "ugearp": {
+        "translation": "ugearp",
+        "id": "49",
         "color": "#6D4C41"
     },
 };
